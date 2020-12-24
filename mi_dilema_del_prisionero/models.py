901f04bd9f430e5e64c18d53ae81dcd06a69f294
Ricardo Diaz <ricardo.andres.diaz@correounivalle.edu.co>
2020-12-23 22:22:25 -0500

    Subiendo archivos a Heroku

---
+++ mi_dilema_del_prisionero/models.py
@@ -0,0 +1,88 @@
+from otree.api import (
+    models,
+    widgets,
+    BaseConstants,
+    BaseSubsession,
+    BaseGroup,
+    BasePlayer,
+    Currency as c,
+    currency_range,
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'mi_dilema_del_prisionero'
+    players_per_group = 2
+    num_rounds = 2
+    pago_ambos_cooperan = -1
+    pago_ambos_traicionan = -2
+    pago_diferente_traiciona = 0
+    pago_diferente_coopera = -3
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    traiciona = models.BooleanField(
+        label="Por favor, escoja la opción",
+        choices = [[True, "Traiciona"],
+                [False, "Coopera"]]
+        )
+    acepta_terminos = models.BooleanField(
+        label="Por favor, indique si acepta los términos de esta actividad",
+        choices=[[True, "Sí"],
+                [False, "No"],
+                ],
+        default = True        
+        ) 
+    numero_identificacion = models.IntegerField(label="Por favor, ingrese su número de identificación")
+    nombre_completo = models.StringField(label="Por favor, escriba su nombre completo")
+    institucion_educativa = models.StringField(
+        label="Indique la institución a la que pertenece",
+        choices = [
+            ["Universidad del Valle", "Universidad del Valle"],
+            ["Universidad del Rosario", "Universidad del Rosario"],
+            ["Uniminuto", "Uniminuto"],
+            ["Otro", "Otro"],
+            ],
+        widget = widgets.RadioSelect,    
+        )
+    pregunta_1 = models.IntegerField(
+        label = "¿Qué sucede si ambos cooperan?",
+        choices = [
+            [1, "Ambos somos condenados" ],
+            [2, "Soy condenado a 3 años"],
+            [3, "Salgo libre"],
+            [4, "Ambos sirven un año"]
+            ],
+        widget = widgets.RadioSelect,       
+        )
+    pregunta_2 = models.IntegerField(
+        label = "¿Qué sucede si ambos traicionan?",
+        choices = [
+            [1, "El otro sale libre" ],
+            [2, "Ambos sirven dos años"],
+            [3, "Salgo libre"],
+            [4, "Ambos son condenados a 3 años"]
+            ],
+        widget = widgets.RadioSelect,       
+        )    
+    def pregunta_1_error_message(self, value):
+        if value != 4:
+            return "Respuesta incorrecta. Por favor, lea de nuevo las instrucciones"
+    def pregunta_2_error_message(self, value):
+        if value != 2:
+            return "Respuesta incorrecta. Por favor, lea de nuevo"        
---
+++ settings.py
@@ -0,0 +1,38 @@
+from os import environ
+
+SESSION_CONFIGS = [
+    dict(
+       name='mi_dilema_del_prisionero',
+       display_name="Mi Dilema del Prisionero",
+       num_demo_participants=2,
+       app_sequence=['mi_dilema_del_prisionero']
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = False
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = 'qk5lo89=8k*#01de8(4=g18%=j8py8gm9@+xs$&^-$wl!e#7#e'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
